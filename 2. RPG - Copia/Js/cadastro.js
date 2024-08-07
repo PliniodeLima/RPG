fbaa77fbbb26684695be78e6aa2d31020f210c6c
Plínio de Lima <105227042+PliniodeLima@users.noreply.github.com>
2024-08-06 22:28:40 -0300

    Add files via upload

---
+++ 2. RPG - Copia/Js/cadastro.js
@@ -0,0 +1,15 @@
+document.getElementById('registrationForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Previne o comportamento padrão de envio do formulário
+  
+    const playerName = document.getElementById('playerName').value.trim();
+    const playerClass = document.getElementById('playerClass').value;
+  
+    if (playerName && playerClass) {
+      // Salva os dados do jogador no localStorage
+      localStorage.setItem('playerName', playerName);
+      localStorage.setItem('playerClass', playerClass);
+      // Redireciona para o index.html
+      window.location.href = 'index.html';
+    }
+  });
+  
